ebc52072c146d687ef2528a17e8f9551cdb4816d
cris7M <krishnasanjay010@gmail.com>
2024-08-02 11:18:29 +0530

    2_8_24 code pushed

---
+++ first_lecture/script.js
@@ -0,0 +1,22 @@
+// function showPopup() {
+//   alert("Welcome to code naman");
+// }
+
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log("sum of two numbers is " + c);
+// const
+// var,
+// let
+// //Global
+// const a = 10;
+// {
+//   a = 20;
+//   console.log(a);
+// }
+// console.log("Hello world");
+// item           price  qty  result
+// coconut_oil     10     10
+
+// 10 * 10 = 100
