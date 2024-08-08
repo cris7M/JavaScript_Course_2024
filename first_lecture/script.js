6bdcf48f55bee88d93bb27712cdf4592ec336f10
cris7M <krishnasanjay010@gmail.com>
2024-08-08 10:25:07 +0530

    latest code 8th aug

--- first_lecture/script.js
+++ first_lecture/script.js
@@ -20,3 +20,88 @@
 // coconut_oil     10     10
 
 // 10 * 10 = 100
+// var age = 45;
+// let age = 24;
+
+// console.log(age);
+
+// Primitive data types
+// String
+let name = "sandeep";
+let greeting = `hello, ${name}`;
+
+console.log(greeting);
+
+// Number
+let age = 30;
+let price = 19.99;
+console.log(age, price);
+
+// boolean
+let isStudent = true;
+let hasGraduated = false;
+console.log(isStudent, hasGraduated);
+
+// undefined
+let x;
+console.log(x);
+
+// null
+let y = null;
+console.log(y);
+
+// Symbol
+let sym1 = Symbol("foo");
+let sym2 = Symbol("sandeep");
+console.log(sym1 == sym2);
+
+// BigInt
+let bigInteger = "759843759843759843574398574398574398574359843754398754398";
+console.log(bigInteger);
+
+// composit data type
+// Objects
+// arrays
+// functions
+
+// Objects
+// let person = {
+//   firstName: "sandeep",
+//   secondName: "kumar",
+//   age: 25,
+//   score: 5.6,
+//   ishasGraduated: hasGraduated,
+// };
+
+// console.log(person);
+
+// Arrays
+// let color = ["red", "gree", "blue"];
+// console.log(color[0]);
+
+// function
+// function greet(name) {
+//   console.log(`hello ${name} how are you ?`);
+// }
+// greet("magesh");
+
+// 3. Special data  type
+// Date
+// let currentDate = new Date();
+// console.log(currentDate);
+
+// identifiers
+// var name, function name, class name, object name, array name
+
+// Rules for declaring the idenitfier
+// 1.
+
+// var full  name = "sandeep"
+// var _full@name = "sandeep"
+
+// function $printname(){
+
+// }
+
+// var full_Name
+// var fullName
